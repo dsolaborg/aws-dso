2552410d108010d4983cb1a1ed0f75605b102cd3
clyde.fondop <clyde.fondop@gmail.com>
2018-10-16 10:51:52 -0400

    ready to be tested

--- main.tf
+++ main.tf
@@ -1,35 +1,10 @@
-variable "vpc_cidr" {}
-variable "environment" {}
-variable "region" {}
-variable "bucket_name" {}
-
-provider "aws" {
-  version = "~> 1.0"
-  region  = "${var.region}"
-}
-
-provider "template" {
-  version = "~> 1.0"
-}
-
-variable "private_subnet_cidrs" {
-  type = "list"
-}
-
-variable "public_subnet_cidrs" {
-  type = "list"
-}
-
-variable "availability_zones" {
-  type = "list"
-}
 
 # Backup the terraform tfstate
 data "terraform_remote_state" "tfstate" {
   backend = "s3"
   config {
     bucket = "${var.bucket_name}"
-    key = "${var.environment}/terraform.tfstate"
+    key = "${var.environment}/lab/terraform.tfstate"
     region = "us-east-1"
   }
 }
